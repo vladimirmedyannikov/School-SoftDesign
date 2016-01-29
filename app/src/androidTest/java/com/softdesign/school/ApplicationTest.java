c4c1232a5a62258589c0a2b5d14b32ac29733da3
Vladimir <vladimir.medyannikov@gmail.com>
2016-01-29 23:23:20 +0400

    Hometask #2.

---
+++ app/src/androidTest/java/com/softdesign/school/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.softdesign.school;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * Created by Vladimir on 23.01.2016.
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest(Class<Application> applicationClass) {
+        super(applicationClass);
+    }
+}
